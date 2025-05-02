30cad212d04986876b1e22673b2990d92d920297
LW95x <liam.woodall@live.co.uk>
2025-05-02 22:57:17 +0100

    add product to cart functionality

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,27 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CreateCartItem } from 'src/app/models/cart.model';
 import { Product } from 'src/app/models/product.model';
+import { CartsService } from 'src/app/services/carts.service';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
   productId!: string;
   product!: Product;
+  quantity: number = 1;
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductsService
+    private productService: ProductsService,
+    private cartService: CartsService
   ) {}
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.paramMap.get('id')!;
     this.productService.getProductById(this.productId).subscribe({
-      next: (data) => this.product = data,
-      error: (err) => console.error('Product ID couldnt be found:', err)
+      next: (data) => (this.product = data),
+      error: (err) => console.error('Product ID couldnt be found:', err),
     });
+  }
+
+  handleAddToCart(product: Product, quantity: number): void {
+    const userId = localStorage.getItem('userId');
+
+    const cartItem: CreateCartItem = {
+      productId: product.productId,
+      quantity: quantity,
+    };
+
+    if (userId && product) {
+      this.cartService.addShoppingCartItem(userId, cartItem).subscribe({
+        next: () => {
+          console.log('Product succesfully added to shopping cart.');
+        },
+        error: (err) => {
+          console.error('Failed to add product to shopping cart', err);
+        },
+      });
+    }
+  }
+
+  increment() {
+    this.quantity++;
+  }
+
+  decrement() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  onQuantityChange(value: number) {
+    const max = this.product.quantity ?? 1;
+    this.quantity = Math.min(Math.max(value, 1), max);
   }
 }
