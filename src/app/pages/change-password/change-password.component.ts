0959072f08e6fe136ca8749ae2e0a5c84ef084f3
LW95x <liam.woodall@live.co.uk>
2025-05-07 23:58:51 +0100

    change user password form functionality

--- src/app/pages/change-password/change-password.component.ts
+++ src/app/pages/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-change-password',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent {
+  currentPassword: string = '';
+  newPassword: string = '';
+
+  constructor (private userService: UsersService) {}
+
+  sendPasswordChangeRequest(currentPassword: string, newPassword: string): void {
+    const userId = localStorage.getItem('userId');
+
+    if (userId) {
+      this.userService.updateUserPassword(userId, currentPassword, newPassword).subscribe({
+        next: () => {
+          console.log('User password was succesfully deleted.');
+        },
+        error: (err) => {
+          console.error('Failed to update user password.', err);
+        }
+      })
+    }
+  }
 
 }
