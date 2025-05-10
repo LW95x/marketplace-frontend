5c81a0b12bd7213654f52fe8c0510099abcce151
LW95x <liam.woodall@live.co.uk>
2025-05-10 23:53:18 +0100

    change user avatar functionality

--- src/app/pages/change-avatar/change-avatar.component.ts
+++ src/app/pages/change-avatar/change-avatar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UpdateUser } from 'src/app/models/user.model';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-change-avatar',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-avatar.component.css']
 })
 export class ChangeAvatarComponent {
+  selectedFile: File | null = null;
 
+constructor(private userService: UsersService) {}
+
+onFileSelect(event: Event): void {
+  const file = event.target as HTMLInputElement;
+  if (file.files) {
+    this.selectedFile = file.files[0];
+  }
+}
+
+  sendAvatarChangeRequest(): void {
+    const userId = localStorage.getItem('userId');
+    const jsonPatchDocument: UpdateUser[] = [{
+      op: 'replace',
+      path: '/avatar',
+      value: this.selectedFile
+  }];
+
+    if (userId) {
+      this.userService
+        .updateUser(userId, jsonPatchDocument)
+        .subscribe({
+          next: () => {
+            console.log('User Avatar was succesfully updated.');
+          },
+          error: (err) => {
+            console.error('Failed to update user Avatar.', err);
+          },
+        });
+    }
+  }
 }
