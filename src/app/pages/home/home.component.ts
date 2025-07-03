6758f1163f583c046951eb40feef4b1c26a14cdd
LW95x <liam.woodall@live.co.uk>
2025-07-03 15:34:01 +0100

    navigation amendments for messaging purposes, linking products from home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    products: Product[] = [];
+    userProducts: Product[] = [];
+    productError: string | null = null;
+    bigDealsOfTheDay: Product[] = [];
+    smallDealsOfTheDay: Product[] = [];
+  
+    constructor(private productService: ProductsService) {}
 
+  ngOnInit() {
+    this.loadAllProducts();
+  }
+
+  loadAllProducts(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        console.log('Products succesfully loaded.');
+
+        this.bigDealsOfTheDay = [this.products[0], this.products[3]];
+        this.smallDealsOfTheDay = [this.products[1], this.products[2], this.products[4], this.products[5]];
+      },
+      error: (err) => {
+        this.productError =
+          'Products could not be found.';
+          console.error(err);
+      }
+    })
+  }
 }
--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -1,38 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Message } from 'src/app/models/message.model';
-import { User } from 'src/app/models/user.model';
 import { MessagesService } from 'src/app/services/messages.service';
-import { SignalrService } from 'src/app/services/signalr.service';
-import { UsersService } from 'src/app/services/users.service';
-import { v4 as uuid } from 'uuid'; 
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css'],
 })
-export class MessagesComponent implements OnInit, OnDestroy {
-  messages: Message[] = [];
-  liveMessages: string[] = [];
-  users: User[] = [];
-
-  receiverId: string = '';
-  message: string = '';
+export class MessagesComponent {
+  conversations: Message[] = [];
 
   constructor(
-    private signalRService: SignalrService,
-    private messageService: MessagesService,
-    private userService: UsersService
+    private messageService: MessagesService
   ) {}
 
   ngOnInit(): void {
-    this.signalRService.startConnection();
     this.loadConversations();
-    this.loadAllUsers();
-
-    this.signalRService.onMessageReceived((msg: Message) => {
-      this.messages.push(msg);
-    });
   }
 
   loadConversations(): void {
@@ -41,7 +24,7 @@ export class MessagesComponent implements OnInit, OnDestroy {
     if (userId) {
       this.messageService.getAllConversations(userId).subscribe({
         next: (data) => {
-          this.messages = data;
+          this.conversations = data;
           console.log(`Messages succesfully loaded.`);
         },
         error: (err) => {
@@ -49,45 +32,5 @@ export class MessagesComponent implements OnInit, OnDestroy {
         },
       });
     }
-  }
-
-  loadAllUsers(): void {
-    this.userService.getAllUsers().subscribe({
-      next: (data) => {
-        this.users = data;
-        console.log(`Users succesfully loaded.`);
-      },
-      error: (err) => {
-        console.error('Users could not be loaded.', err);
-      }
-    })
-  }
-
-  sendMessage(receiverId: string, text: string): void {
-    const senderId = localStorage.getItem('userId');
-
-    if (senderId && receiverId && text) {
-      this.signalRService.sendMessage(senderId, receiverId, text).then(() => {
-          console.log('Message successfully sent.');
-          this.messages.push({
-            id: uuid(),
-            content: text,
-            sentTime: new Date(),
-            senderId,
-            receiverId,
-            senderUsername: 'You',
-            receiverUsername: ''
-          })
-          this.message = '';
-        })
-        .catch(err => {
-          console.error('Error sending message.', err);
-        })
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.signalRService.stopConnection();
-    this.signalRService.removeMessageListener();
   }
 }
